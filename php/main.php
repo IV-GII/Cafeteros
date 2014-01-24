fda5d1e9d221b294729724c036079702c8d25710
javierIV <javieriv@MacBook-Pro-de-Javier.local>
2014-01-24 18:58:38 +0100

    php

--- php/main.php
+++ php/main.php
@@ -38,22 +38,40 @@
 
   <body>
 
-    <a href="./img/logo_tu_cafe_online.png"></a>
+  <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="./img/logo_tu_cafe_online.png">Brand</a>
+  </div>
 
-    <div class="container">
-
-      <form class="form-signin" role="form">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="text" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
-
-    </div> <!-- /container -->
-
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="./estado.php">Estado</a></li>
+      <li><a href="./recaudacion.php">Recaudación</a></li>
+    </ul>
+   
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Link</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">NOMBRE USUARIO <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Cerrar sesión</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
