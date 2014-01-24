1fa40ad715feab98e248b143683e48d25562dc8a
javierIV <javieriv@MacBook-Pro-de-Javier.local>
2014-01-24 23:56:10 +0100

    paneles maquinas

--- php/main.php
+++ php/main.php
@@ -38,40 +38,53 @@
 
   <body>
 
-  <nav class="navbar navbar-default" role="navigation">
-  <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="./img/logo_tu_cafe_online.png"> Cafeteros </a>
+    <nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href=".main.php">Estado</a></li>
+        <li><a href=".mantenimiento.php">Mantenimiento</a></li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Cerrar Sesión</a></li>
+          </ul>
+        </li>
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </nav>
+
+  <div class="panel panel-success">
+    Aquí primera máquina de café.
+    En esta máquina todo esta OK. VERDE.
+
   </div>
 
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="./estado.php">Estado</a></li>
-      <li><a href="./mantenimiento.php">Mantenimiento</a></li>
-    </ul>
-   
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">NOMBRE USUARIO <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Cerrar sesión</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Separated link</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div><!-- /.navbar-collapse -->
-</nav>
+  <div class="panel panel-warning">
+    Aquí segunda máquina de café.
+    En esta máquina hay algo que revisar. AMARILLO.
+  </div>
+
+  <div class="panel panel-danger">
+    Aquí tercera máquina de café.
+    En esta máquina hay algo mal. ROJO.
+  </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
