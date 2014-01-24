7ec8a3ca70dc7aa6686fbb868e80d2d11e2483e5
javierIV <javieriv@MacBook-Pro-de-Javier.local>
2014-01-25 00:51:45 +0100

    ultimos cambios main

--- php/main.php
+++ php/main.php
@@ -38,7 +38,6 @@
   </head>
 
   <body>
-
     <nav class="navbar navbar-default" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
