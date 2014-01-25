a3d8328b65df380a16826e8631b5e5cfd3877a00
elmendacorp <elmendacorp@gmail.com>
2014-01-25 20:44:59 +0100

    asd

--- php/main.php
+++ php/main.php
@@ -39,15 +39,16 @@
 
   <body>
     <?php
-    connection = new mysqli(
-    $_ENV['OPENSHIFT_MYSQL_DB_HOST'],
-    $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'],
-    $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'],
-    $_ENV['OPENSHIFT_APP_NAME'], // By default, app name == db name
-    $_ENV['OPENSHIFT_MYSQL_DB_PORT'],
-    $_ENV['OPENSHIFT_MYSQL_DB_SOCKET']
-  );
-?>
+    define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+    define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+    define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+    define('DB_BASE',getenv('OPENSHIFT_APP_NAME'));
+    define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+
+    define('DB_DSN' , 'mysql:dbname='.DB_BASE.';host='.DB_HOST.';port='.DB_PORT.'');
+    define("DB_USUARIO", DB_USER);
+        define("DB_CONTRASENIA", DB_PASS);
+    ?>
     <nav class="navbar navbar-default" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
