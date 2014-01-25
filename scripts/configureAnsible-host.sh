d2121e2e73f6440c3432d85773d88d51aabf56b0
Antonio Guirola <antonioguirola@gmail.com>
2014-01-25 16:20:53 +0100

    scripts en su versión inicial añadidos

--- scripts/configureAnsible-host.sh
+++ scripts/configureAnsible-host.sh
@@ -1,6 +1,9 @@
 sudo pip install paramiko PyYAML jinja2 httplib2 ansible
-echo "192.168.1.65" > ~/ansible_hosts
-export ANSIBLE_HOSTS=~/ansible_hosts
 sudo apt-get install -y ssh-copy-id
 ssh-copy-id -i ~/.ssh/id_rsa.pub pi@192.168.1.65
-
+# Descargar proyecto cafeteros y archivo con el host
+cd ~
+git@github.com:IV-GII/Cafeteros.git
+cd cafeteros
+#Provisionar la máquina
+ansible-playbook scripts/raspiPlaybook.yml -i scripts/ansibleHosts -u pi
