d3db88b59a27a57e38dac6d0c9389964fb05b321
elmendacorp <elmendacorp@gmail.com>
2014-01-25 20:50:11 +0100

    asdasd

--- php/main.php
+++ php/main.php
@@ -38,6 +38,17 @@
   </head>
 
   <body>
+  <?php
+  define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+    define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+    define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+    define('DB_BASE',getenv('OPENSHIFT_APP_NAME'));
+    define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+
+    define('DB_DSN' , 'mysql:dbname='.DB_BASE.';host='.DB_HOST.';port='.DB_PORT.'');
+    define("DB_USUARIO", DB_USER);
+        define("DB_CONTRASENIA", DB_PASS);
+    ?>
     <nav class="navbar navbar-default" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
