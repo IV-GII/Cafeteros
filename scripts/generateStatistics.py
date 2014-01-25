dabcc0c60124e75986e3aeba34460cf4508872a8
Antonio Guirola <antonioguirola@gmail.com>
2014-01-25 20:01:47 +0100

    cambios en el script de lectura por puerto serie

--- scripts/generateStatistics.py
+++ scripts/generateStatistics.py
@@ -1,15 +1,34 @@
 import serial 
-serial = serial.Serial("/dev/ttyUSB0", 9600, bytesize=8)
+serial = serial.Serial("/dev/ttyUSB0", baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=1)
  
 f = open("status.txt", "w")
- 
+
+string=""
+
+cont=0
+inicioPasado=False
+
 while True:
 #uses the serial port
   data = serial.read()
-  print data
+  string += data
+  #print data
   f.write(data)
-  #f.flush()
+  f.flush()
+  #print("Contador: "+str(cont))
+  #print (string)
+  if data=="=":
+  	cont+=1
+  else:
+  	cont=0
+
+  if cont==20 and inicioPasado==False:
+  	inicioPasado=True
+  elif cont==20 and inicioPasado==True:
+  	break
  
 f.close()
+
+print (string)
 
 exit()
