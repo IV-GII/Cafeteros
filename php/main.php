3ec5e6e13274eedc45ec73bf98c605c02220da3e
javierIV <javieriv@MacBook-Pro-de-Javier.local>
2014-01-25 11:21:20 +0100

    closes #14

--- php/main.php
+++ php/main.php
@@ -71,7 +71,7 @@
   <div id="contenedor_margen">
     <div class="row">
 
-      <div class= "col-lg-3 col-md-3 col-sm-12 col-xs-12">
+      <div class= "col-lg-3 col-md-3 col-sm-3 col-xs-12">
         <ul class="nav nav-pills nav-stacked">
           <li class="active"><a href="#">Máquina uno COD MAQUINA</a></li>
           <li><a href="#">Máquina dos COD MAQUINA</a></li>
@@ -80,7 +80,7 @@
       </div>
       
 
-      <div class= "col-lg-9 col-md-9 col-sm-12 col-xs-12">
+      <div class= "col-lg-9 col-md-9 col-sm-9 col-xs-12">
         <div class="panel panel-success">
           <div class="panel-heading">
             <h3 class="panel-title">Código Máquina 1</h3>
