79308541ff9478631a632cac9cd9d59c39401764
javierIV <javieriv@MacBook-Pro-de-Javier.local>
2014-01-26 12:10:31 +0100

    Final

--- php/main.php
+++ php/main.php
@@ -175,7 +175,7 @@
                   </div>
                 </div>
 
-                <div class= "col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                <div class= "col-lg-4 col-md-4 col-sm-4 col-xs-12">
                   <div class="panel panel-info">
                     <div class="panel-heading">
                       <h4 class="panel-title">
@@ -225,7 +225,7 @@
                   </div>
                 </div>
 
-                <div class= "col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                <div class= "col-lg-4 col-md-4 col-sm-4 col-xs-12">
                   <div class="panel panel-info">
                     <div class="panel-heading">
                       <h4 class="panel-title">
