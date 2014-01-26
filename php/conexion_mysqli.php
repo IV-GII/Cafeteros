ba4ee59544b4f719b02ee0d14bc37407b25596fc
javierIV <javieriv@MacBook-Pro-de-Javier.local>
2014-01-26 01:16:59 +0100

    Configurando base datos

---
+++ php/conexion_mysqli.php
@@ -0,0 +1,63 @@
+
+<?php
+	require_once "configuracion.php";
+	ini_set ("display_errors","1" );
+	error_reporting(E_ALL);
+
+	function conectarse(){
+		$mysqli = new mysqli(DB_DSN, DB_USUARIO, DB_PASS, DB_NAME) or die ("No se ha podido Conectar");
+		
+		if ($mysqli->connect_errno) {
+		    printf("Connect failed: %s\n", $mysqli->connect_error);
+		    exit();
+		}
+		return $mysqli;		
+	}
+
+	function consultarMaquinas($maquina){
+		$mysqli = conectarse(); 
+		$maquinas=""; 
+		if ($resultado = $mysqli->query("SELECT * FROM " . MAQUINAS . "WHERE cod_maquina=" . $maquina)) {
+			while ($row = $resultado->fetch_assoc()) {
+				$maquinas= 'Modelo: '.$row=["modelo"].'<br>Firm: '.$row=["firmware"].'<br>Fabricante: '.$row=["fabricante"].'<br>N&uacute;mero de cafes: '.$row=["n_cafes"].'<br>Vasos totales: '.$row=["vasos_totales"].'<br>Vasos mantenido: '.$row=["vasos_mantenido"].'<br>N&uacute;mero impresi&oacute;n: '.$row=["n_impresion"];
+			}
+			return $maquinas;
+		}else{
+			echo "error peticion";
+			printf("Errormessage: %s\n", $mysqli->error);
+		}
+		
+		$mysqli->close();
+	}
+
+	function consultarCodigosMaquinas(){
+		$mysqli = conectarse(); 
+		$columnaIzq=""; 
+		if ($resultado = $mysqli->query("SELECT * FROM " . MAQUINAS)) {
+			while ($row = $resultado->fetch_assoc()) {
+				$columnaIzq= '<li><a href="#">M&aacute;quina '.$row=["cod_maquina"].'</a></li>'.columnaIzq;
+			}
+			return $maquinas;
+		}else{
+			echo "error peticion";
+			printf("Errormessage: %s\n", $mysqli->error);
+		}
+		
+		$mysqli->close();
+	}
+
+	function consultarCodigo($maquina){
+		$mysqli = conectarse(); 
+		$codigo=""; 
+		if ($resultado = $mysqli->query("SELECT * FROM " . MAQUINAS . "WHERE cod_maquina=" . $maquina)) {
+			while ($row = $resultado->fetch_assoc()) {
+				$codigo= 'M&aacute;quina '.$row=["cod_maquina"];
+			}
+			return $codigo;
+		}else{
+			echo "error peticion";
+			printf("Errormessage: %s\n", $mysqli->error);
+		}
+		
+		$mysqli->close();
+	}
---
+++ php/configuracion.php
@@ -0,0 +1,10 @@
+<?php
+	error_reporting(E_ALL);
+	define("DB_DSN", "127.6.12.130" ); 
+	define("DB_NAME", "cafeteros");
+	define("DB_USUARIO", "adminkVRFJrB" ); 
+	//define("DB_USUARIO", "root" ); 
+	define("DB_PASS", "gDLVUniSBVss" );
+	define("TABLA_USUARIO", "usuario" );
+	define("MAQUINAS", "maquinas");
+?>
