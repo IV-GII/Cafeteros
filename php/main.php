3bd506ce7597181431b4ef2b3a0de3ae23bdf0df
javierIV <javieriv@MacBook-Pro-de-Javier.local>
2014-01-26 10:38:51 +0100

    sesiones ok?

--- php/main.php
+++ php/main.php
@@ -13,9 +13,10 @@
   }
   if(consultarUsuario($user, $password)){
     $_SESSION['username']=$user;
-  }else{
-    header("Location: index.php");
   }
+  // }else{
+  //   header("Location: index.php");
+  // }
 
 ?>
 
